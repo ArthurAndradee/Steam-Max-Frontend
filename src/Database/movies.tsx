7bd572811ac695233adee21bc3663e0f775d68b6
arthur andrade <arthurandrad.23@gmail.com>
2024-08-10 20:05:58 -0300

    Feat: Added movies objects array

--- src/Database/movies.tsx
+++ src/Database/movies.tsx
@@ -1,6 +1,62 @@
-export const movies = 
-[
+export const movies = [
     {
-        
+        title: "Inception",
+        mainCast: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+        genre: "Sci-Fi",
+        banner: "https://www.movieposters.com/cdn/shop/files/inception.mpw.123395_9e0000d1-bc7f-400a-b488-15fa9e60a10c_480x.progressive.jpg?v=1708527589"
     },
-]
+    {
+        title: "The Dark Knight",
+        mainCast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+        genre: "Action",
+        banner: "https://www.movieposters.com/cdn/shop/files/darkknight.building.24x36_20e90057-f673-4cc3-9ce7-7b0d3eeb7d83_480x.progressive.jpg?v=1707491191"
+    },
+    {
+        title: "The Matrix",
+        mainCast: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+        genre: "Sci-Fi",
+        banner: "https://www.movieposters.com/cdn/shop/products/ed4796ac6feff9d2a6115406f964c928_6b200bda-fe71-4900-ad7f-903cdda50dab_480x.progressive.jpg?v=1573587596"
+    },
+    {
+        title: "Forrest Gump",
+        mainCast: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
+        genre: "Drama",
+        banner: "https://www.movieposters.com/cdn/shop/products/77e810938fdaf6a8a6879aabef828d78_7b4c41f9-0c33-4b0e-acb3-93bad47e4347_480x.progressive.jpg?v=1573587341"
+    },
+    {
+        title: "The Godfather",
+        mainCast: ["Marlon Brando", "Al Pacino", "James Caan"],
+        genre: "Crime",
+        banner: "https://www.movieposters.com/cdn/shop/products/godfather.132843.mp_480x.progressive.jpg?v=1654286052"
+    },
+    {
+        title: "Pulp Fiction",
+        mainCast: ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+        genre: "Crime",
+        banner: "https://www.movieposters.com/cdn/shop/products/ab401c136cca10812cda5ac64c3f7c2e_bb5e62f7-b34f-4547-b5c7-495cc2dd1bd9_480x.progressive.jpg?v=1573591339"
+    },
+    {
+        title: "The Lord of the Rings: The Fellowship of the Ring",
+        mainCast: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"],
+        genre: "Fantasy",
+        banner: "https://www.movieposters.com/cdn/shop/files/ItemP2658_jpg_480x.progressive.jpg?v=1692302023"
+    },
+    {
+        title: "Gladiator",
+        mainCast: ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+        genre: "Action",
+        banner: "https://www.movieposters.com/cdn/shop/files/gladiator_ver1_480x.progressive.jpg?v=1721061607"
+    },
+    {
+        title: "Titanic",
+        mainCast: ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"],
+        genre: "Romance",
+        banner: "https://www.movieposters.com/cdn/shop/products/dc27b3438cb1465c9594a90bd8bcd723_b4c9371f-9f23-49fe-ae9b-6e4da4b1efce_480x.progressive.jpg?v=1573588576"
+    },
+    {
+        title: "Jurassic Park",
+        mainCast: ["Sam Neill", "Laura Dern", "Jeff Goldblum"],
+        genre: "Adventure",
+        banner: "https://www.movieposters.com/cdn/shop/products/4940c5878babf3dc5d2ca567b7558178_9e62fc4c-4116-48e5-a4f5-3a99c73ae7b1_480x.progressive.jpg?v=1573651499"
+    }
+];
