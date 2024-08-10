6b0566f54908ad82a9eb6dd99c209b18952918ff
arthur andrade <arthurandrad.23@gmail.com>
2024-08-10 20:55:36 -0300

    Style: Styled movies slider

--- src/Components/Titles-Row/Title-Slider/title-slider.tsx
+++ src/Components/Titles-Row/Title-Slider/title-slider.tsx
@@ -1,44 +1,38 @@
 import Slider from 'react-slick';
 import './title-slider.css';
+import { movies } from '../../../Database/movies';
+import { useEffect, useState } from 'react';
 
 function TitleSlider() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const slidesToShow = windowWidth < 600 ? 4 : windowWidth < 820 ? 5 : windowWidth < 950 ? 6 : windowWidth < 1150 ? 7 : windowWidth < 1366 ? 8 : windowWidth < 1640 ? 9 : 10;
+  
   const settings = {
-    dots: true,
+    className: "center",
+    dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 6
+    slidesToShow: slidesToShow,
+    slidesToScroll: slidesToShow
   };
+
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
+    <div className="slider-container mx-5">
+      <Slider {...settings} className='mx-4'>
+      {movies.map((movie) => (
+          <div >
+            <img src={movie.banner} alt={movie.title} className='title-card' />
+            <div style={{width:'1px'}} className='border'></div>
+          </div>
+        ))}
       </Slider>
     </div>
   );
