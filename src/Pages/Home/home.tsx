8033d5e366cbae8dc80f76b42c5eaa56bd9c39aa
Arthur Andrade <arthurandrad.23@gmail.com>
2024-08-15 09:05:47 -0300

    Feat: Implemented request from backend in index

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -1,41 +1,24 @@
-import { useState, useEffect } from 'react';
 import Header from '../../Components/Headers/Standard/header';
 import TitleSlider from '../../Components/Titles-Row/Title-Slider/title-slider';
 import { userProfiles } from '../../Database/profiles';
 import { Movie } from '../../utils/interfaces';
 import './home.css';
 
-function Home() {
-  const [romanceMovies, setRomanceMovies] = useState<Movie[]>([]);
-  const [fantasyMovies, setFantasyMovies] = useState<Movie[]>([]);
-  const [horrorMovies, setHorrorMovies] = useState<Movie[]>([]);
+interface HomeProps {
+  romanceMovies: Movie[];
+  fantasyMovies: Movie[];
+  horrorMovies: Movie[];
+}
 
-  useEffect(() => {
-    const fetchMoviesByCategory = async (category: string, setMovies: React.Dispatch<React.SetStateAction<Movie[]>>) => { 
-      try {
-        const response = await fetch(`http://localhost:5000/movies/${category}`);
-        const data = await response.json();
-        setMovies(data);
-      } catch (error) {
-        console.error(`Error fetching ${category} movies:`, error);
-      }
-    };
-
-    fetchMoviesByCategory('Romance', setRomanceMovies);
-    fetchMoviesByCategory('Fantasy', setFantasyMovies);
-    fetchMoviesByCategory('Horror', setHorrorMovies);
-  }, []);
-
-  console.log(romanceMovies)
-
+function Home({ romanceMovies, fantasyMovies, horrorMovies }: HomeProps) {
   return (
     <div className='home-main'>
       <Header currentProfilePicture={userProfiles[0].userPicture} />
-      <div className='mx-5 my-2 ps-4 text-light display-6' style={{fontSize:'1rem'}}>Romance</div>
+      <div className='mx-5 my-2 ps-4 text-light display-6' style={{ fontSize: '1rem' }}>Romance</div>
       <TitleSlider movies={romanceMovies} />
-      <div className='mx-5 my-2 ps-4 text-light display-6' style={{fontSize:'1rem'}}>Fantasia</div>
+      <div className='mx-5 my-2 ps-4 text-light display-6' style={{ fontSize: '1rem' }}>Fantasia</div>
       <TitleSlider movies={fantasyMovies} />
-      <div className='mx-5 my-2 ps-4 text-light display-6' style={{fontSize:'1rem'}}>Horror</div>
+      <div className='mx-5 my-2 ps-4 text-light display-6' style={{ fontSize: '1rem' }}>Horror</div>
       <TitleSlider movies={horrorMovies} />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Home from './Pages/Home/home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Movie } from './utils/interfaces';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-]);
+const fetchMoviesByCategory = async (category: string): Promise<Movie[]> => {
+  try {
+    const response = await fetch(`http://localhost:5000/movies/${category}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching ${category} movies:`, error);
+    return [];
+  }
+};
+
+const App = () => {
+  const [romanceMovies, setRomanceMovies] = useState<Movie[]>([]);
+  const [fantasyMovies, setFantasyMovies] = useState<Movie[]>([]);
+  const [horrorMovies, setHorrorMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const [romance, fantasy, horror] = await Promise.all([
+        fetchMoviesByCategory('Romance'),
+        fetchMoviesByCategory('Fantasy'),
+        fetchMoviesByCategory('Horror'),
+      ]);
+      setRomanceMovies(romance);
+      setFantasyMovies(fantasy);
+      setHorrorMovies(horror);
+    };
+
+    fetchMovies();
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home romanceMovies={romanceMovies} fantasyMovies={fantasyMovies} horrorMovies={horrorMovies} />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to   start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
