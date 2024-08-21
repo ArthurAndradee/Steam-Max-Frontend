da15abd9bc232b487307851f1bad3b16ab17887a
arthur andrade <arthurandrad.23@gmail.com>
2024-08-21 01:58:25 -0300

    Feat: Finished Profile Picker css

--- src/Components/Profile-Picker/Profile-Bubble/profile-bubble.tsx
+++ src/Components/Profile-Picker/Profile-Bubble/profile-bubble.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { ProfileBubbleProps } from '../../../utils/interfaces';
 import './profile-bubble.css';
 
 function ProfileBubble(user: ProfileBubbleProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className='profile-picker-container' onClick={user.onClick}>
-      <div className='profile-background-hover'/>
-      <div className='profile-picker-main'>
-          <img className='profile-picker-picture'  alt='Profile' src={user.userPicture} />
+    <div
+      className={`profile-picker-container ${isHovered ? 'hovered' : ''}`}
+      onClick={user.onClick}
+    >
+      <div className='profile-picker-main'       onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
+        <img className='profile-picker-picture' alt='Profile' src={user.userPicture}  />
         <div className='profile-picker-name'>{user.userName}</div>
       </div>
     </div>
