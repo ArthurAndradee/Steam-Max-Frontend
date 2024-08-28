22f7992fe3c227545583396fcd33f9125b2ada96
arthur andrade <arthurandrad.23@gmail.com>
2024-08-28 14:06:25 -0300

    Fix: Fixed video player

--- src/Pages/Title-Player/player.tsx
+++ src/Pages/Title-Player/player.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { PlayerProps } from '../../utils/interfaces/components';
+import { Video } from "reactjs-media";
 import './player.css';
 
 function Player({ trailerUrl, title }: PlayerProps) {
@@ -14,7 +15,7 @@ function Player({ trailerUrl, title }: PlayerProps) {
             clearTimeout(timeoutId);
             timeoutId = setTimeout(() => {
                 setIsCursorMoving(false);
-            }, 2500);
+            }, 1500);
         };
 
         window.addEventListener('mousemove', handleMouseMove);
@@ -29,11 +30,16 @@ function Player({ trailerUrl, title }: PlayerProps) {
         <div className="trailer-container">
             <div className="trailer-player">
                 <h2 className={`text-light display-3 ps-4 pt-4 player-title ${isCursorMoving ? 'visible' : 'hidden'}`}>{title}</h2>
-                <div className="video-container justify-content-center my-5">
-                    <video controls width="100%">
-                        <source src={trailerUrl} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                <div className="video-container my-5">
+                <Video
+                    src={trailerUrl}
+                    controls={true}
+                    height={1080}
+                    width={1920}
+                    poster={
+                      "https://hips.hearstapps.com/hmg-prod/images/ripley-pa-108-011822-01629-r-661067043d66f.jpg?resize=980:*"
+                    }
+                />
                 </div>
             </div>
         </div>
