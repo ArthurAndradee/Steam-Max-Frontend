7bc04a2c209a329524c8ad5058c7c2bd10828f76
Arthur Andrade <arthurandrad.23@gmail.com>
2024-08-30 10:54:15 -0300

    Feat: Implemented Dynamical Logic for login/sign in redirect button

--- src/Components/Headers/Login/welcome.tsx
+++ src/Components/Headers/Login/welcome.tsx
@@ -1,17 +1,26 @@
-import { useNavigate } from 'react-router';
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './welcome.css';
 
 function WelcomeHeader() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/';
+
+  const buttonText = isHomePage ? 'Sign In' : 'Log In';
+  const navigateTo = isHomePage ? '/register' : '/';
 
   return (
     <div className='p-5 d-flex'>
       <h2 className='text-danger'>StreamMax</h2>
-      <select className="form-select w-25  ms-auto select-language-input" aria-label="Default select example">
+      <select className="form-select w-25 ms-auto select-language-input" aria-label="Default select example">
         <option value="English">English</option>
         <option value="Portugues">Português</option>
       </select>
-      <button className='btn btn-danger ms-4' onClick={() => navigate('/register')}>Sign In</button>
+      <button className='btn btn-danger ms-4' onClick={() => navigate(navigateTo)}>
+        {buttonText}
+      </button>
     </div>
   );
 }
